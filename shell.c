4be4d8997a948abba755a7cf86a6e9a14ba46cdf
Dennis Miring'u <dennismiringu03@gmail.com>
2024-02-27 13:15:39 +0300

    all two

--- shell.c
+++ shell.c
@@ -1,30 +1,44 @@
 #include "shell.h"
 
-int main(void) {
-    char *line = NULL;
-    char **args = NULL;
-    int status;
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-    do{
-	    display_prompt();
-	    free(line);
-	    line = read_input();
-	    if (line == NULL) {
-		    break;
-	    }
-	    free(args);
-	    args = parse_input(line);
-	    if (args == NULL) {
-		    continue;
-	    }
-	    status = execute_command(args);
-    } while (status);	
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-    return 0;
-    }
-
-void display_prompt() {
-    printf("($) ");
-    fflush(stdout);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
